5497ebacda475645703e939ba904b7ad42fea467
Cotea Ionut <coteaionutroberto@gmail.com>
2025-10-23 21:33:06 +0300

    tema2

---
+++ Lab1/Assignment2/header1.h
@@ -0,0 +1,11 @@
+//
+// Created by cotea on 10/23/2025.
+//
+
+#ifndef HEADER1_H
+#define HEADER1_H
+#include <iostream>
+inline void f() {
+    std::cout << "Function f() from header1\n";
+}
+#endif
---
+++ Lab1/Assignment2/header2.h
@@ -0,0 +1,11 @@
+//
+// Created by cotea on 10/23/2025.
+//
+
+#ifndef HEADER2_H
+#define HEADER2_H
+#include <iostream>
+inline void f() {
+    std::cout << "Function f() from header2\n";
+}
+#endif
